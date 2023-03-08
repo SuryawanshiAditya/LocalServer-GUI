0d43174a5b7607c472ce742105c07d8ee045bdb6
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-03-08 21:03:06 +0530

    refactor upload script

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,24 +1,32 @@
-// elements 
-let controlBtn = document.getElementById('control-btn')
+// components
+let controlBtn = document.getElementById("control-btn");
 
 // set control button state
-controlBtn.addEventListener('click', () => {
-    let state = checkControlState('control-btn')
-    if (state == "enabled") {
-        controlBtn.innerText = "Start Server"
-        controlBtn.style.background = "rgb(87, 205, 168)"
-    } else if (state == "disabled") {
-        controlBtn.innerText = "Terminate Server"
-        controlBtn.style.background = "#f36760"
-    }
-})
+controlBtn.addEventListener("click", () => {
+  let state = checkControlState("control-btn");
+  if (state == "up") {
+    terminateServer();
+  } else if (state == "down") {
+    startServer();
+  }
+});
 
-// helper functions 
+// helper functions
 function checkControlState(id) {
-    let controlBtn = document.getElementById(id)
-    if (controlBtn.innerText == "Start Server") {
-        return "disabled"
-    } else if (controlBtn.innerText == "Terminate Server") {
-        return "enabled"
-    }
+  let controlBtn = document.getElementById(id);
+  if (controlBtn.innerText == "Start Server") {
+    return "down";
+  } else if (controlBtn.innerText == "Terminate Server") {
+    return "up";
+  }
+}
+
+function startServer() {
+  controlBtn.innerText = "Terminate Server";
+  controlBtn.style.background = "#f36760";
+}
+
+function terminateServer() {
+  controlBtn.innerText = "Start Server";
+  controlBtn.style.background = "rgb(87, 205, 168)";
 }
